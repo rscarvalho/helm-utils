a11bea3cb99c51672d41f00077469c17a01d3366
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2018-12-03 08:17:44 -0500

    Separating the data structures in their own file

--- pkg/helm/helm_client.go
+++ pkg/helm/helm_client.go
@@ -7,38 +7,6 @@ import (
 	"os/exec"
 )
 
-// StatusType is an enumeration of the valid status for a release
-type StatusType uint8
-
-const (
-	// DEPLOYED is the status that denotes a release has been deployed
-	DEPLOYED StatusType = iota + 1
-)
-
-type status struct {
-	Code      StatusType
-	Resources string
-}
-
-type helmTimestamp struct {
-	Seconds int64
-	Nanos   int64
-}
-
-type info struct {
-	Description   string
-	Status        *status
-	FirstDeployed *helmTimestamp `json:"first_deployed"`
-	LastDeployed  *helmTimestamp `json:"last_deployed"`
-}
-
-// ReleaseStatus represents the status of a release in Helm
-type ReleaseStatus struct {
-	Name      string
-	Namespace string
-	Info      *info
-}
-
 // GetReleaseStatus gets a status for a release
 func GetReleaseStatus(releaseName string) (*ReleaseStatus, error) {
 	statusBytes, err := runHelm("status", releaseName)
